e2bec015031041587e71180e9041acb017da2173
Ishika Arora <ishikaarora4@gmail.com>
2018-11-20 18:40:43 -0500

    update weather pull python script

---
+++ src/model/model.R
@@ -0,0 +1,28 @@
+library(Amelia)
+library(dplyr)
+
+weather <- read.csv("data/EPA_weather_2016.csv")
+weather_na <- weather %>% filter(!is.na(Average.Air.Pressure))
+length(unique(weather$County))
+
+data <- read.csv("data/data_merged_weather-dropped.csv")
+
+mod <- lm(AQI ~ Average.Temperature + Average.Air.Pressure + Average.Humidity + Average.Windspeed + Total.Emissions + CO2 + Methane + Nitrous.Oxide + HFC + PFC + SF6 + NF3 + HFE + Short.Lived.Compounds + Income + Population + pp_net_gen_MWh, data=data)
+summary(mod)
+
+sapply(data,function(x) sum(is.na(x)))
+sapply(data$Region,function(x) unique(x, na.rm=TRUE))
+
+max(data$Region, na.rm = TRUE)
+missmap(data, main = "Missing values vs observed")
+
+rm(data)
+
+bk <- read.csv("data/weather_data_berkshire.csv")
+bk1 <- read.csv("data/weather_data_berkshire1.csv")
+
+bk2 <- weather %>% filter(state.name)
+sapply(bk,function(x) sum(is.na(x)))
+sapply(bk1,function(x) sum(is.na(x)))
+
+bk$altimeter_set_1 - bk1$altimeter_set_1
